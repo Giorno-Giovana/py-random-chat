907b052ab4b8c6dd37eb4cf8ff33b34f75e38bbb
senago <mirzoyan.oganes@mail.ru>
2021-11-20 15:18:28 +0300

    Stage

---
+++ frontend/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <NuxtLink :to="{ name: 'index' }">duck</NuxtLink>
+    </div>
+
+    <div class="user">
+      <div class="info">
+        <div class="list">
+          <NuxtLink :to="{ name: 'login' }">Log in</NuxtLink>
+          <NuxtLink :to="{ name: 'register' }">Register</NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script> 
