a3ccd2a7b37fa088b06f55f5a962ab722a37abd6
Denis Mazur <denismazur8@gmail.com>
2021-11-20 15:13:12 +0300

    add room streaming support

--- backend/internal/service/registry.go
+++ backend/internal/service/registry.go
@@ -9,6 +9,7 @@ import (
 type Registry struct {
 	AuthService AuthService
 	UserService UserService
+	RoomService RoomService
 }
 
 func NewRegistry(log *logrus.Entry, repository *db.Repository) *Registry {
@@ -16,6 +17,7 @@ func NewRegistry(log *logrus.Entry, repository *db.Repository) *Registry {
 
 	registry.AuthService = NewAuthService(log, repository)
 	registry.UserService = NewUserService(log, repository)
+	registry.RoomService = NewRoomService(log)
 
 	return registry
 }
