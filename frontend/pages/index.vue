6015609e92b6a65c89be68e94c827990b3939ed6
senago <mirzoyan.oganes@mail.ru>
2021-11-20 12:36:35 +0300

    hotfix

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -36,11 +36,14 @@
         </div>
       </div>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import videoStream from "~/components/video-stream.vue";
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
 const servers = {
   iceServers: [
     {
