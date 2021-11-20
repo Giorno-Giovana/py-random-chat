73554ce67b616815bbc71a8abe9771ea456c6aff
senago <mirzoyan.oganes@mail.ru>
2021-11-20 13:47:45 +0300

    Refactor recognition

--- recognition/app.py
+++ recognition/app.py
@@ -1,8 +1,13 @@
-from flask import Flask, request, Response
-from api import api
+from flask_cors import CORS
+from flask import Flask, Response, request, jsonify
+from internal.recognition import FaceRecognition
+from utils import error_response, file_to_bytesio
+from werkzeug.exceptions import BadRequest
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-app.register_blueprint(api)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
@@ -10,5 +15,31 @@ def index():
     return Response('stub')
 
 
+@app.route('/upload_image', methods=['POST'])
+@cross_origin()
+def user_upload_photo():
+    file = request.files['image']
+    username = request.form['username']
+    if file:
+        FaceRecognition().upload(username, file)
+        return Response('uploaded an image'), 200
+    else:
+        return error_response('failed to retrieve image', BadRequest.code)
+
+
+@app.route('/identify_image', methods=['POST'])
+@cross_origin()
+def image_identify():
+    file = request.files['image']
+    if file:
+        username = FaceRecognition().identify(file)
+        response = jsonify({
+            'username': username,
+        })
+        return response, 200
+    else:
+        return error_response('failed to retrieve image', BadRequest.code)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', threaded=True)
+    app.run(debug=True, host='0.0.0.0', threaded=True, port=2000)
