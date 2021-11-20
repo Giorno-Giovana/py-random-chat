9ea1bd75f0fe5c5f6319a65e73a0c75fe215a6da
Giorno-Giovana <zaverclub@gmail.com>
2021-11-20 16:34:25 +0300

    Сделаны комнаты тотемные

--- frontend/pages/rooms/_room.vue
+++ frontend/pages/rooms/_room.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <div>Hi mark</div>
     <VideoStream :stream="localStream" />
-    <VideoStream :stream="rs" v-for="rs in remoteStreams" :key="rs.id" />
-    <input class="border border-black" type="text" v-model="callerId" />
-    <button class="border border-black" @click="call">Call</button>
+    <div class="grid-cols-4">
+      <VideoStream :stream="rs.stream" v-for="rs in remoteStreams" :key="rs.stream.id" />
+    </div>
   </div>
 </template>
 
@@ -22,20 +21,11 @@ export default {
     return {
       localStream: undefined,
       remoteStreams: [],
-      callerId: "",
       id: peer.id,
+      room: this.$route.params.room,
     };
   },
-  room: undefined,
-  beforeDestroy() {
-    peer.disconnect();
-  },
   async mounted() {
-    const rooms = await this.$fire.firestore
-      .collection("rooms")
-      .doc(this.$route.params.room);
-    this.$options.peers = rooms.collection("peers");
-
     this.openPeerConnection();
     this.localStream = await window.navigator.mediaDevices.getUserMedia({
       video: true,
@@ -43,31 +33,62 @@ export default {
     });
     this.subscribeToIncomingCalls();
   },
+  socket: undefined,
   methods: {
-    call() {
-      console.log("Попытка позвонить", this.callerId);
-      const call = peer.call(this.callerId, this.localStream);
-      call.on("stream", this.addStreamToRemotes);
-    },
     openPeerConnection() {
       peer.on("open", async (id) => {
-        this.$options.peers.add({ peer: this.id });
+        console.log("Локальный id", id);
         this.id = id;
-        console.log("Локальный id", this.id);
+        this.$options.socket = new WebSocket(`ws://localhost:8080/api/room/join?pid=${id}&rid=${this.room}`);
+        this.subscribeToSocket()
       });
+    },
+    subscribeToSocket() {
+      this.$options.socket.onopen = () => {
+        console.log("[open] Соединение установлено");
+      };
+
+      this.$options.socket.onmessage = event => {
+        const data = JSON.parse(event.data)
+        if (data.type === 1) {
+          this.call(data.peer)
+        }
+        if (data.type === 2) {
+          this.removeUserFromCall(data.peer)
+        }
+      };
+
+      this.$options.socket.onclose = (event) => {
+        if (event.wasClean) {
+          console.log(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+        } else {
+          console.log('[close] Соединение прервано');
+        }
+      };
+
+      this.$options.socket.onerror = (error) => {
+        console.log(`[error] ${error.message}`);
+      };
     },
     subscribeToIncomingCalls() {
       console.log("Подписка на входящие звонки");
       peer.on("call", (call) => {
-        console.log("Совершена попытка звонка", call);
         call.answer(this.localStream);
-        call.on("stream", this.addStreamToRemotes);
+        call.on("stream", stream => this.addStreamToRemotes(stream, call.peer));
       });
     },
-    addStreamToRemotes(stream) {
-      if (this.remoteStreams.some((s) => s.id === stream.id)) return;
-      console.log("Оп, подрубочка", stream);
-      this.remoteStreams.push(stream);
+    call(p) {
+      console.log("Зовнок", p);
+      const call = peer.call(p, this.localStream);
+      call.on("stream", stream => this.addStreamToRemotes(stream, p));
+    },
+    addStreamToRemotes(stream, p) {
+      if (this.remoteStreams.some((r) => r.stream.id === stream.id)) return;
+      this.remoteStreams.push({stream, peer: p});
+    },
+    removeUserFromCall(peer) {
+      console.log('Удалён ', peer)
+      this.remoteStreams = this.remoteStreams.filter(r => r.peer !== peer)
     },
   },
 };
