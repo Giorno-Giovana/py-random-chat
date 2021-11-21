08c02416c2a20ba125401c5aeff59a529569e210
Denis Mazur <denismazur8@gmail.com>
2021-11-21 05:16:23 +0300

    move api to new uri

--- frontend/services/tinder.js
+++ frontend/services/tinder.js
@@ -1,4 +1,4 @@
 const getParticipant = (userId, participationMode) => {
-    const socket = new WebSocket(`ws://sshamanism.ru:8080/api/tinder/next?uid=${userId}&participation_mode=${participationMode}`);
+    const socket = new WebSocket(`wss://sshamanism.ru:8080/api/api/tinder/next?uid=${userId}&participation_mode=${participationMode}`);
     socket.onmessage = (event) => console.log(event)
 };
