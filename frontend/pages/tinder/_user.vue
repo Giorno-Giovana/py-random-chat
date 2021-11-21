61d0d5dea0490d762a8f640dd98e94791f65889b
Giorno-Giovana <zaverclub@gmail.com>
2021-11-21 03:41:46 +0300

    Набросок для тиндера

---
+++ frontend/pages/tinder/_user.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>You</div>
+    <div>Me</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "_user",
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
